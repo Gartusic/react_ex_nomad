602fb198cf251ae162334b501be042a9f8ef33e8
Gartusic <ljh148454@gmail.com>
2022-01-09 17:37:15 +0900

    배포문제 해결, MovieApp파일 App에 병합

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Button from "./Button";
 import CoinTracker from "./CoinTracker";
 import TodoList from "./TodoList";
-import MovieApp from "./MovieApp";
 import styles from "./App.module.css";
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
+import React from "react";
+import { //npm i react-router-dom 을 통해 설치한 후 라우터를 사용할 수 있똬
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+//npm install gh-pages = github에서 제공하는 무료 서비스 - 도메인을 가진 웹페이지를 배포할 수 있도록 해줌
+// 설치 후 npm run deploy 하면 댐,,
+// 그전에 깃헙에서 호스팅하기 위해서 pakage.json에서 deploy와predeploy를 설정하고 
+//homepage를 깃헙 호스팅 형식에 맞는 도메인으로 작성함
+
 
 import {useEffect, useState} from "react";
 
@@ -26,12 +39,6 @@ function Hello(){
   useEffect(hiFn,[]);
   return <h1>Hello</h1>;
 }
-
-
-
-
-
-
 
 
 
@@ -83,9 +90,14 @@ function App() {
         <CoinTracker />
       </div>
       <hr />
-      <div>
-        <MovieApp />
-      </div>
+      <Router basename={process.env.PUBLIC_URL}>
+           <Routes>
+               <Route path="/movie/:id" element={<Detail />}>
+               </Route>
+               <Route path="/" element={<Home />}>
+               </Route>
+           </Routes>
+       </Router>
     </div>
   );
 }
